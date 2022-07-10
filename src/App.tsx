b79aad688746e2aa279ac3fa9327db8683cdf8b2
KeplerBoyce <keplerboy@gmail.com>
2022-07-09 23:08:26 -0700

    styling change for small windows

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import { useEffect, useState } from "react";
 import Sidebar from "./components/Sidebar";
+import Timer from "./components/Timer";
 
 export default function App() {
     return (
         <div className="w-full h-[100vh] bg-dark-1">
             <Sidebar className="absolute left-0 bg-dark-0" />
+            <Timer />
         </div>
     );
 }
