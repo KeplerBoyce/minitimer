b28a20791aeb3238fa5cff6b1fd0457b5f47764e
KeplerBoyce <keplerboy@gmail.com>
2022-07-09 22:14:02 -0700

    formatting

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,8 @@
-export default function Sidebar(props: {className: string}) {
+export default function Sidebar(props: {className?: string}) {
+    const {className} = props;
+
     return (
-        <div className={"w-72 h-full p-2 " + props.className} >
+        <div className={"w-72 h-full p-2 " + className} >
             <div className="flex justify-center text-3xl border-b-2">
                 <span className="text-green-500">mini</span>
                 <span className="text-white">timer</span>
