69f3215f01d0bc64d1a95148be5c9d9d0d95f259
KeplerBoyce <keplerboy@gmail.com>
2022-07-11 00:40:02 -0700

    solve modal fix

--- src/components/Solve.tsx
+++ src/components/Solve.tsx
@@ -8,10 +8,12 @@ export default function Solve(props: {solve: SolveType, removeSolve: (index: num
     const [isOpen, setIsOpen] = useState(false);
 
     return (
-        <div onClick={() => setIsOpen(true)} className={"flex text-lg text-light text-left " + className}>
-            <span className={widths[0]}>{solve.index}</span>
-            <span className={widths[1]}>{msToTime(solve.millis)}</span>
+        <>
+            <div onClick={() => setIsOpen(true)} className={"flex text-lg text-light text-left " + className}>
+                <span className={widths[0]}>{solve.index}</span>
+                <span className={widths[1]}>{msToTime(solve.millis)}</span>
+            </div>
             <SolveModal solve={solve} removeSolve={() => removeSolve(solve.index)} isOpen={isOpen} setIsOpen={setIsOpen} />
-        </div>
+        </>
     )
 }
