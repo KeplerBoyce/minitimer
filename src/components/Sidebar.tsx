6692af96bfff0ff42ed053908f93bfc727fc5a16
KeplerBoyce <keplerboy@gmail.com>
2022-07-11 00:20:38 -0700

    sessions dropdown menu

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -7,8 +7,10 @@ export default function Sidebar(props: {solves?: SolveType[], setSolves: (solves
     const {solves, setSolves, className} = props;
     const [sessions, setSessions] = useState([undefined as unknown as SessionType]);//list of all sessions
     const [chosenSession, setChosenSession] = useState({} as SessionType);
-    const [isOpen, setIsOpen] = useState(false);//for session modal
+    const [isOpen, setIsOpen] = useState(false);//for session info modal
+    const [menuOpen, setMenuOpen] = useState(false);//for session selection menu
 
+    //load sessions from localStorage; if there are none, create an empty one
     useEffect(() => {
         if (!sessions[0]) {
             const ls = localStorage.getItem("sessions");
@@ -46,10 +48,24 @@ export default function Sidebar(props: {solves?: SolveType[], setSolves: (solves
                 <span className="text-green-500">mini</span>
                 <span className="text-white">timer</span>
             </div>
+
             <div className="flex justify-center gap-2 mb-2">
-                <button onClick={() => {}} className="bg-light px-1 rounded-lg">
-                    <p className="text-2xl -translate-y-1">⌄</p>
-                </button>
+                <div className="relative inline-block">
+                    <button
+                        onClick={() => {setMenuOpen(!menuOpen)}}
+                        className={"z-20 bg-light px-1" + (menuOpen ? " rounded-t-lg" : " rounded-lg")}
+                    >
+                        <p className="text-2xl -translate-y-1">⌄</p>
+                    </button>
+                    <div
+                        onClick={() => {setMenuOpen(!menuOpen)}}
+                        className={"hover:cursor-pointer absolute left-0 right-0 h-2 bg-light" + (menuOpen ? " visible" : " invisible")}
+                        />
+                    <div className={"absolute left-0 mt-2 w-48 h-48 bg-light rounded-lg rounded-tl-none"
+                        + (menuOpen ? " visible" : " invisible")}>
+
+                    </div>
+                </div>
                 <button onClick={() => setIsOpen(true)} className="bg-light px-3 py-1 rounded-lg">
                     {chosenSession.name}
                 </button>
@@ -57,21 +73,29 @@ export default function Sidebar(props: {solves?: SolveType[], setSolves: (solves
                     {chosenSession.cube}
                 </button>
             </div>
+
             <div className="flex flex-col-reverse w-full">
-                {solves?.map(s =>
-                    <Solve
-                        key={s.index}
-                        solve={{...s}}
-                        removeSolve={removeSolve}
-                        widths={["w-1/5", "w-4/5"]}
-                        className="px-2 hover:bg-dark-2 hover:cursor-pointer text-light text-lg font-mono"
-                    />
+                {solves ? (
+                    solves.map(s =>
+                        <Solve
+                            key={s.index}
+                            solve={{...s}}
+                            removeSolve={removeSolve}
+                            widths={["w-1/5", "w-4/5"]}
+                            className="px-2 hover:bg-dark-2 hover:cursor-pointer text-light text-lg font-mono"
+                        />
+                    )
+                ) : (
+                    <p className="px-2 text-lg text-light italic">
+                        Session empty
+                    </p>
                 )}
                 <div className="flex px-2 text-lg text-light font-bold text-left border-b border-light">
                     <span className="w-1/5 italic">#</span>
                     <span className="w-4/5">Time</span>
                 </div>
             </div>
+
             <SessionModal session={chosenSession} removeSession={removeSession} isOpen={isOpen} setIsOpen={setIsOpen} />
         </div>
     )
