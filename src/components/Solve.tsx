89c3a8d4f09376ede1c73f48327fc9778f35a610
Kepler Boyce <keplerboy@gmail.com>
2023-08-06 23:48:44 -0700

    fix issue with duplicate times in ao5

--- src/components/Solve.tsx
+++ src/components/Solve.tsx
@@ -12,17 +12,18 @@ export default function Solve(props: {
     const { solve, lastFive, widths, onClick, className } = props;
 
     const ao5 = () => {
-        if (lastFive.length == 0) return "--";
-        let arr = lastFive.map(s => s.millis);
-        arr = arr.filter(x => x !== Math.min(...arr) && x !== Math.max(...arr));
+        if (lastFive.length < 5) return "--";
+        const arr = lastFive.map(s => s.millis);
+        arr.splice(arr.indexOf(Math.min(...arr)), 1);
+        arr.splice(arr.indexOf(Math.max(...arr)), 1);
         return msToTime(arr.reduce((a, b) => a + b) / 3);
     }
 
     return (
         <div onClick={onClick} className={"flex text-lg text-light text-left " + className}>
-            <span className={widths[0]}>{solve.index}</span>
-            <span className={widths[1]}>{msToTime(solve.millis)}</span>
-            <span className={widths[2]}>{ao5()}</span>
+            <p className={widths[0]}>{solve.index}</p>
+            <p className={widths[1]}>{msToTime(solve.millis)}</p>
+            <p className={widths[2]}>{ao5()}</p>
         </div>
     )
 }
